4cc8f5c1681847cee96fb98a174f854bd7e3fe0f
judgementine <42780618+judgementine@users.noreply.github.com>
2018-12-01 05:32:59 -0600

    kjhg

--- graphics_examples/SDL_SA_img/main.cpp
+++ graphics_examples/SDL_SA_img/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <assert.h>
+#include <time.h>
 using namespace std;
 
 //include SDL header
@@ -20,7 +21,7 @@ struct Pixel
 
 struct Square
 {
-	float x, y, w, h;
+	int x, y, w, h;
 
 	Pixel color;
 
@@ -36,6 +37,8 @@ namespace Game
 	unsigned char *keys = NULL;
 	SDL_Window *window = NULL;
 
+	SDL_Surface *original;
+	SDL_Surface *Image;
 	SDL_Surface *scratch;
 	SDL_Surface *tmp;
 	SDL_Surface *screen;
@@ -45,7 +48,88 @@ namespace Game
 	Pixel *color;
 	int n_walkers = 100;
 	Square * squares;
-	int n_boxes = 200;
+	int n_boxes = 1000;
+	double fitness = 0.0;
+	float temperature = 1;
+	float permute_amount = 204.0;//<-- VERY IMPORTANT RANK 3
+	float temperature_decay = 0.95;//<-- VERY IMPORTANT RANK 1
+	int n_permute_same_temp = 80;//<-- VERY IMPORTANT RANK 2
+
+
+	void permuteBoxes()
+	{
+
+		int n_permute = 1;
+		//for (int i = 1; i < n_shapes-1; i++)
+		for (int i = 0; i < n_permute; i++)
+		{
+			int k = rand() % (n_boxes - 1);
+			float x_amount = permute_amount * (1.0 - 2.0*rand() / RAND_MAX);
+			float y_amount = permute_amount * (1.0 - 2.0*rand() / RAND_MAX);
+
+			squares[k].x += x_amount;
+			squares[k].y += y_amount;
+
+			if (squares[k].x < 0) squares[k].x = 0;
+			if (squares[k].y < 0) squares[k].y = 0;
+			if (squares[k].x >screen_width-1) squares[k].x = screen_width-1;
+			if (squares[k].y > screen_height-1) squares[k].y = screen_height-1;
+		}
+	}
+	double difference(SDL_Surface * a, SDL_Surface * b)
+	{
+		double difference = 0.0;
+		Pixel * o = (Pixel *)a->pixels;
+		Pixel * p = (Pixel *)b->pixels;
+		for (int i = 0; i < screen_height * screen_width; i++)
+		{
+				difference += pow(o[i].r - p[i].r, 2) + pow(o[i].g - p[i].g, 2) + pow(o[i].b - p[i].b, 2)/* + pow(o[i].a - p[i].a, 2)*/;
+		}
+		difference = difference / (screen_width*screen_height);
+		return difference;
+
+	}
+
+	void blend(SDL_Surface * a, Square * d)
+	{
+		Pixel * p = (Pixel *)a->pixels;
+		int * visited = new int[screen_width*screen_height];
+		for (int i = 0; i < screen_width*screen_height;i++)
+		{
+			visited[i] = 0;
+		}
+		for (int i = 0; i < n_boxes; i++)
+		{
+			for (int j = d[i].y; j < d[i].y + d[i].h;j++)
+			{
+				for (int k = d[i].x; k < d[i].x + d[i].w; k++)
+				{
+					p[j * screen_width + k].r = (p[j * screen_width + k].r + d[i].color.r) / 2;
+					p[j * screen_width + k].g = (p[j * screen_width + k].g + d[i].color.g) / 2;
+					p[j * screen_width + k].b = (p[j * screen_width + k].b + d[i].color.b) / 2;
+					/*
+					p[j * screen_width + k].r += d[i].color.r;
+					p[j * screen_width + k].g += d[i].color.g;
+					p[j * screen_width + k].b += d[i].color.b;*/
+					//p[j * screen_width + k].a += d[i].color.a;
+					//visited[j * screen_width + k]+= 1;
+				}
+			}
+		}
+		/*
+		for (int i = 0; i < screen_width*screen_height;i++)
+		{
+			if (visited[i] != 0)
+			{
+				p[i].r = p[i].r / visited[i];
+				p[i].g = p[i].g / visited[i];
+				p[i].b = p[i].b / visited[i];
+				//p[i].a = p[i].a / visited[i];
+			}
+
+		}*/
+	}
+
 
 	void init()
 	{
@@ -55,7 +139,7 @@ namespace Game
 		keys = (unsigned char*)SDL_GetKeyboardState(NULL);
 
 		window = SDL_CreateWindow(
-			"Fortnite",
+			"SDL Image Simulated Annealing",
 			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 			screen_width, screen_height, SDL_WINDOW_SHOWN);
 
@@ -63,17 +147,23 @@ namespace Game
 			-1, SDL_RENDERER_SOFTWARE);
 
 
-		scratch = SDL_CreateRGBSurfaceWithFormat(0, screen_width, screen_height, 24, SDL_PIXELFORMAT_RGB24);
+
+		squares = (Square*)malloc(n_boxes * sizeof(Square));
+		original = IMG_Load("mickey_mouse.jpg");
+		printf("%d\n", original->format->BitsPerPixel);
+		Image = SDL_CreateRGBSurfaceWithFormat(0, screen_width,screen_height, 24, SDL_PIXELFORMAT_RGB24);
+		SDL_BlitScaled(original, NULL, Image, NULL);
+		tmp = SDL_CreateRGBSurfaceWithFormat(0, screen_width, screen_width, 24, SDL_PIXELFORMAT_RGB24);
+		scratch = SDL_CreateRGBSurfaceWithFormat(0, screen_width, screen_width, 24, SDL_PIXELFORMAT_RGB24);
 
 		screen = SDL_GetWindowSurface(window);
-
+		/*
 		grid = (int*)malloc(scratch->w*scratch->h * sizeof(int));
 		memset(grid, 0, sizeof(int)*scratch->w*scratch->h);
 
 		px = (int*)malloc(n_walkers * sizeof(int));
 		py = (int*)malloc(n_walkers * sizeof(int));
-		color = (Pixel*)malloc(n_walkers * sizeof(Pixel));
-		squares = (Square*)malloc(n_walkers * sizeof(Square));
+		color = (Pixel*)malloc(n_walkers * sizeof(Pixel));*
 
 		for (int i = 0; i < n_walkers; i++)
 		{
@@ -84,20 +174,24 @@ namespace Game
 			color[i].b = rand() % 256;
 		}
 
+		color[0].r = 0;
+		color[0].g = 0;
+		color[0].b = 0;*/
+
 		for (int i = 0; i < n_boxes; i++)
 		{
-			squares[i].w = rand() % 100;
-			squares[i].h = rand() % 80;
-			squares[i].x = rand() % (screen_width-100);
-			squares[i].y = rand() % (screen_height-80);
+			squares[i].w = rand() % 50;
+			squares[i].h = rand() % 50;
+			squares[i].x = rand() % (screen_width-50);
+			squares[i].y = rand() % (screen_height-50);
 			squares[i].color.r = rand() % 256;
 			squares[i].color.g = rand() % 256;
 			squares[i].color.b = rand() % 256;
-			squares[i].color.a = rand() % 256;
+			squares[i].color.a = 255;
 		}
-		color[0].r = 0;
-		color[0].g = 0;
-		color[0].b = 0;
+
+		blend(scratch, squares);
+		fitness = difference(Image, scratch);
 		
 	}
 
@@ -116,7 +210,28 @@ namespace Game
 				exit(0);
 			}
 		}
+		SDL_BlitSurface(scratch, NULL, tmp, NULL);
+		permuteBoxes();
+		blend(tmp, squares);
+		double tmpFitness = difference(Image, tmp);
+		float p = exp((tmpFitness - fitness) / temperature);
+		float r = (double)rand() / RAND_MAX;
+		if (tmpFitness < fitness)
+		{
 
+			SDL_BlitSurface(tmp, NULL, scratch, NULL);
+			fitness = tmpFitness;
+		}
+		else if (r < p)
+		{
+			
+			SDL_BlitSurface(tmp, NULL, scratch, NULL);
+			fitness = tmpFitness;
+		}
+		temperature *= temperature_decay;
+		cout << "Fitness: " << fitness << endl;
+
+		/*
 		for (int i = 0; i < n_walkers; i++)
 		{
 
@@ -143,13 +258,14 @@ namespace Game
 			}
 
 			grid[py[i] *screen_width + px[i]] = i;
-		}
+		}*/
 	}
 
 
 	void draw_1()
 	{
 		//clear screen with white
+		/*
 		for (int i = 0; i < scratch->w*scratch->h; i++)
 		{
 			Pixel *p = (Pixel *)scratch->pixels;
@@ -157,32 +273,33 @@ namespace Game
 			p[i].r = color[grid[i]].r;
 			p[i].g = color[grid[i]].g;
 			p[i].b = color[grid[i]].b;
-		}
+		}*/
 
 		SDL_BlitScaled(scratch, NULL, screen, NULL);
+
+		//clear screen with white
+		for (int i = 0; i < scratch->w*scratch->h; i++)
+		{
+			Pixel *p = (Pixel *)scratch->pixels;
+
+			p[i].r = 0;
+			p[i].g = 0;
+			p[i].b = 0;
+		}
+		
+
 
 		SDL_UpdateWindowSurface(window);
 
 	}
 
-	double difference(SDL_Surface * a, SDL_Surface * b)
-	{
-		a->pixels;
-	}
 
-	void blend(SDL_Surface * a, Square  d)
-	{
-		Pixel * p = (Pixel *)a->pixels;
-		for (int i = 0; i < d.w; i++)
-		{
-			squares[i].x
-		}
-	}
 
 }
 int main(int argc, char **argv)
 {
-	
+
+	srand(time(NULL));
 
 	Game::init();
 
